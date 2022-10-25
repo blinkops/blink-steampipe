615fa18b29b44aac1c6364be26805cd848e2cae8
Daniel Kuperman <daniel@blinkops.com>
2022-10-25 17:40:11 +0300

    Download and extract content as prerequisite to steampipe csv/terraform queries

---
+++ scripts/generators/storage.go
@@ -0,0 +1,74 @@
+package generators
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	blinkaws "github.com/blinkops/blink-steampipe/internal/aws"
+	"github.com/pkg/errors"
+)
+
+const (
+	SteampipeStorageFileIdentifier = "STORAGE_FILE_IDENTIFIER"
+	SteampipeStorageAWSKeyID       = "STORAGE_AWS_KEY_ID"
+	SteampipeStorageAWSSecretKey   = "STORAGE_AWS_SECRET_KEY"
+	SteampipeStorageAWSBucket      = "STORAGE_AWS_BUCKET"
+	SteampipeStorageAWSRootDir     = "STORAGE_AWS_ROOT_DIR"
+	SteampipeStorageAWSToken       = "STORAGE_AWS_TOKEN"
+	SteampipeStorageAWSRegion      = "STORAGE_AWS_REGION"
+	storageDestination             = "/storage"
+	tempDesitnation                = "/tmp"
+)
+
+type StorageCredentialsGenerator struct{}
+
+func (gen StorageCredentialsGenerator) Generate() error {
+	fileID, ok := os.LookupEnv(SteampipeStorageFileIdentifier)
+	if !ok {
+		return nil
+	}
+	awsAccessKeyID, ok := os.LookupEnv(SteampipeStorageAWSKeyID)
+	if !ok {
+		return errors.New("missing STORAGE_AWS_KEY_ID env. key")
+	}
+	awsSecretKey, ok := os.LookupEnv(SteampipeStorageAWSSecretKey)
+	if !ok {
+		return errors.New("missing STORAGE_AWS_SECRET_KEY env. key")
+	}
+	bucket, ok := os.LookupEnv(SteampipeStorageAWSBucket)
+	if !ok {
+		return errors.New("missing SteampipeStorageAWSBucket env. key")
+	}
+	region, ok := os.LookupEnv(SteampipeStorageAWSRegion)
+	if !ok {
+		return errors.New("missing SteampipeStorageAWSBucket env. key")
+	}
+	rootDir, ok := os.LookupEnv(SteampipeStorageAWSRootDir)
+	if !ok {
+		return errors.New("missing STORAGE_AWS_ROOT_DIR env. key")
+	}
+	token, ok := os.LookupEnv(SteampipeStorageAWSToken)
+	if !ok {
+		return errors.New("missing STORAGE_AWS_ROOT_DIR env. key")
+	}
+
+	if err := blinkaws.DownloadFileFromS3Bucket(filepath.Join(rootDir, fileID),
+		tempDesitnation,
+		region,
+		bucket,
+		awsAccessKeyID,
+		awsSecretKey,
+		token); err != nil {
+		return errors.New("download content for steampipe")
+	}
+
+	sourcePath := filepath.Join(tempDesitnation, fileID)
+
+	cmd := exec.Command("tar", "-xzf", sourcePath, "-C", storageDestination)
+	if _, err := cmd.Output(); err != nil {
+		return errors.Wrap(err, "extract user content")
+	}
+
+	return nil
+}
