50d7e6f745e78a4a91407ef15ae4c04797b5c8f9
blink <blink@tests.blinkops.com>
2023-05-31 17:33:30 +0300

    added git cloning in the case of custom mod

--- scripts/consts/consts.go
+++ scripts/consts/consts.go
@@ -6,10 +6,12 @@ const (
 )
 
 const (
-	ReportFileParentDirEnvVar       = "REPORT_FILE_PARENT_DIR"
-	ReportFilePathEnvVar            = "REPORT_FILE_NAME"
-	FileOutputOverrideFormat        = "output written to file with identifier '%s'"
-	FileOutputOverrideOnErrorFormat = FileOutputOverrideFormat + " on error"
+	ReportFileParentDirEnvVar              = "REPORT_FILE_PARENT_DIR"
+	ReportFilePathEnvVar                   = "REPORT_FILE_NAME"
+	SteampipeReportCustomModLocationEnvVar = "CUSTOM_MOD_LOCATION"
+	SteampipeCustomModAlias                = "custom-mod"
+	FileOutputOverrideFormat               = "output written to file with identifier '%s'"
+	FileOutputOverrideOnErrorFormat        = FileOutputOverrideFormat + " on error"
 )
 
 const (
--- scripts/main.go
+++ scripts/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/blinkops/blink-steampipe/internal/logger"
 	"github.com/blinkops/blink-steampipe/internal/response_wrapper"
 	"github.com/blinkops/blink-steampipe/scripts/consts"
 	"github.com/blinkops/blink-steampipe/scripts/generators"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -20,7 +22,7 @@ func main() {
 
 	for _, credentialGenerator := range generators.Generators {
 		if err := credentialGenerator.Generate(); err != nil {
-			log.Errorf("failed generate credentials: %v", err)
+			log.Errorf("Failed to generate credentials: %v", err)
 			response_wrapper.HandleResponse("", logger.GetLogs(), "", true)
 			os.Exit(0)
 		}
@@ -33,11 +35,21 @@ func main() {
 	}
 
 	if cmdName == "" {
-		log.Error("no command provided")
+		log.Error("No command provided")
 		response_wrapper.HandleResponse("", logger.GetLogs(), action, true)
 		os.Exit(0)
 	}
 	cmdArgs := os.Args[2:]
+
+	// if a mod location was provided,
+	// we clone the repo to the corresponding location mentioned in queryV2PreInvoke in controller
+	if modLocation := os.Getenv(consts.SteampipeReportCustomModLocationEnvVar); modLocation != "" {
+		if err := cloneMod(modLocation); err != nil {
+			log.Errorf("Failed to load the custom Steampipe mod from %s. Please ensure the provided repository is publicly available.", modLocation)
+			response_wrapper.HandleResponse("", logger.GetLogs(), action, true)
+			os.Exit(0)
+		}
+	}
 
 	cmd := exec.Command(cmdName, cmdArgs...)
 	output, err := cmd.CombinedOutput()
@@ -45,11 +57,19 @@ func main() {
 	// some steampipe benchmark ("check") may return an error code but return a result.
 	// in such a case, we don't want the entire report to fail and display the result.
 	if err != nil && (action != consts.CommandCheck || len(output) == 0) {
-		log.Errorf("execute command: %v", err)
+		log.Errorf("Failed to execute command: %v", err)
 		response_wrapper.HandleResponse(string(output), logger.GetLogs(), action, true)
 		os.Exit(0)
 	}
 
 	response_wrapper.HandleResponse(string(output), logger.GetLogs(), action, false)
 	os.Exit(0)
+}
+
+func cloneMod(repo string) error {
+	cmd := exec.Command("git", "clone", repo, filepath.Join(consts.SteampipeBasePath, consts.SteampipeCustomModAlias), "--quiet")
+	if err := cmd.Run(); err != nil {
+		return errors.Wrap(err, cmd.String())
+	}
+	return nil
 }
